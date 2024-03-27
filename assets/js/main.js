05420ea2118d272c45639caf67324a91e9e7c93a
Manuel Nieto <nietosoftwaredeveloper@outlook.com>
2024-03-26 21:46:43 -0500

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ window.addEventListener('scroll', scrollHeader)
 const btns = document.querySelectorAll(".slider__bg-navBtn");
 const slides = document.querySelectorAll(".video__slide");
 
-var sliderNav = function(manual){
+var sliderNav = function (manual) {
     btns.forEach((btn) => {
         btn.classList.remove("active");
     });
@@ -56,7 +56,7 @@ var sliderNav = function(manual){
     slides[manual].classList.add("active");
 }
 
-btns.forEach((btn, i) =>{
+btns.forEach((btn, i) => {
     btn.addEventListener("click", () => {
         sliderNav(i);
     });
@@ -69,25 +69,25 @@ let swiperPopular = new Swiper(".popular__container", {
     loop: true,
     spaceBetween: 24,
     slidesPerView: 'auto',
-    grabCursor:true,
+    grabCursor: true,
 
 
     pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
+        el: ".swiper-pagination",
+        dynamicBullets: true,
     },
 
     breakpoints: {
-        
+
         768: {
-          slidesPerView: 3,
+            slidesPerView: 3,
 
         },
         1024: {
-          
-          spaceBetween: 48,
+
+            spaceBetween: 48,
         },
-      },
+    },
 });
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
@@ -105,19 +105,19 @@ let mixerFeatured = mixitup('.featured__content', {
 
 const linkFeatured = document.querySelectorAll('.featured__item');
 
-function activeFeatured(){
-    linkFeatured.forEach(l=>l.classList.remove('active-featured'));
+function activeFeatured() {
+    linkFeatured.forEach(l => l.classList.remove('active-featured'));
     this.classList.add('active-featured');
 }
 
-linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured));
+linkFeatured.forEach(l => l.addEventListener('click', activeFeatured));
 
 
 /*=============== SHOW SCROLL UP ===============*/
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -125,17 +125,17 @@ window.addEventListener('scroll', scrollUp)
 
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
@@ -159,15 +159,15 @@ const sr = ScrollReveal({
 
 
 sr.reveal(`.home__title, .popular__container, .features__img, .featured__filters`);
-sr.reveal(`.home__subtitle`, {delay: 500});
-sr.reveal(`.home__elec`, {delay: 600});
-sr.reveal(`.home__img`, {delay: 800});
-sr.reveal(`.home__car-data, .footer__copy`, {delay: 900, interval: 100, origin: 'bottom'});
-sr.reveal(`.home__button`, {delay: 1000, origin: 'bottom'});
+sr.reveal(`.home__subtitle`, { delay: 500 });
+sr.reveal(`.home__elec`, { delay: 600 });
+sr.reveal(`.home__img`, { delay: 800 });
+sr.reveal(`.home__car-data, .footer__copy`, { delay: 900, interval: 100, origin: 'bottom' });
+sr.reveal(`.home__button`, { delay: 1000, origin: 'bottom' });
 
-sr.reveal(`.about__group, .offer__data`, {origin: 'left'});
-sr.reveal(`.about__data, .offer__img, .home__social-icon`, {origin: 'right'});
+sr.reveal(`.about__group, .offer__data`, { origin: 'left' });
+sr.reveal(`.about__data, .offer__img, .home__social-icon`, { origin: 'right' });
 
-sr.reveal(`.features__map, .slider__bg`, {delay:600, origin: 'bottom'});
-sr.reveal(`.features__card`, {interval: 300});
-sr.reveal(`.featured__card, .logos__content, .footer__content`, {interval: 100});
+sr.reveal(`.features__map, .slider__bg`, { delay: 600, origin: 'bottom' });
+sr.reveal(`.features__card`, { interval: 300 });
+sr.reveal(`.featured__card, .logos__content, .footer__content`, { interval: 100 });
