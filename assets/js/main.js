34de64231194aaa910b9af6748289c3ad4c033b6
Manuel Nieto <nietosoftwaredeveloper@outlook.com>
2024-04-23 22:57:54 -0500

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,6 @@ linkFeatured.forEach(l => l.addEventListener('click', activeFeatured));
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if (this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
